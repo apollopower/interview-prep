2bad37acc395f3ea7c6e6f1cc0ce238005afe261
Jonas <erthaljonas@gmail.com>
2019-02-19 15:12:14 -0500

    Added Lua directory

---
+++ lua_practice/hello.lua
@@ -0,0 +1,13 @@
+-- Defines a factorial function
+
+function fact (n)
+    if n == 0 then
+        return 1
+    else
+        return n * fact(n - 1)
+    end
+end
+
+print("Enter a number")
+a = io.read("*number") -- read a number
+print(fact(a))
